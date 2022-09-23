88cd4014f03a8ec1875d887cff17e0df4e7931fe
MBisovskyi <mbisovskyi@gmail.com>
2022-09-23 19:39:20 -0400

    Background image added + Pen image added + NavBar component created

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,7 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+  return <div className="navbar-wrap"></div>;
+};
+
+export default NavBar;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,16 @@
+import NavBar from "../../components/NavBar/NavBar";
 import "./HomePage.css";
+import background from "../../assets/desk.jpg";
+import pen from "../../assets/pen.png";
 
 const HomePage = () => {
   return (
     <div className="homepage-wrap">
-      <div className="left-container"></div>
+      <img className="background-img" src={background} alt="background" />
+      <img className="pen-img" src={pen} alt="pen" height="350px" />
+      <div className="left-container">
+        <NavBar />
+      </div>
       <div className="right-container"></div>
     </div>
   );
