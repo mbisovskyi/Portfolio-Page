fc8906d2938537f0c164defe3fa264534f43c8b6
MBisovskyi <mbisovskyi@gmail.com>
2022-09-23 18:44:52 -0400

    Installed react-router-dom + created HomePage + Positioned two containers on the HomePage + Added styling to visualize containers on the HomePage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <p>Hello, portfolio!</p>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="homepage-wrap">
+      <div className="left-container"></div>
+      <div className="right-container"></div>
+    </div>
+  );
+};
+
+export default HomePage;
