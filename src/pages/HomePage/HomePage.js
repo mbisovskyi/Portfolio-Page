815fadf1e26351841fea57ef0914f863cb5f2562
MBisovskyi <mbisovskyi@gmail.com>
2022-09-25 15:06:07 -0400

    Person picture added + Person signature and person title added and styled

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,7 +1,18 @@
+//Importing styles
 import "./HomePage.css";
+//Importing assets
+import person from "../../assets/Headshot-wb.png";
 
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="person-signature-container">
+        <p className="person-signature-text">Mykola Bisovskyi</p>
+        <p className="person-title-text">Full Stack Software Developer</p>
+      </div>
+      <img className="person-img" src={person} height="500px" />
+    </div>
+  );
 };
 
 export default HomePage;
