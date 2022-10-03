078d6324abf3e3f03ac6f2b7b97e020d5084162c
MBisovskyi <mbisovskyi@gmail.com>
2022-10-02 20:58:15 -0400

    Fix: Added another project object

--- src/components/DisplayProjects/projectsData.js
+++ src/components/DisplayProjects/projectsData.js
@@ -1,16 +1,17 @@
 //Importing projects images
 import musicLibraryImage from "./assets/music-library.png";
 import youtubeCloneImage from "./assets/youtube-clone.png";
+import videoGameDataImage from "./assets/video-game-data.png";
 
 const musicLibrary = {
-  name: "Music Library",
+  name: "MusiColl",
   backendLink: "https://github.com/mbisovskyi/Music-Library-API",
   frontendLink: "https://github.com/mbisovskyi/Music_Library-frontend-",
   image: musicLibraryImage,
 };
 
 const youtubeClone = {
-  name: "YouTube Clone",
+  name: "VideoSmacker",
   backendLink:
     "https://github.com/mbisovskyi/YouTube-Clone-FullStack/tree/main/backend",
   frontendLink:
@@ -18,6 +19,13 @@ const youtubeClone = {
   image: youtubeCloneImage,
 };
 
-const projectsData = [musicLibrary, youtubeClone];
+const videoGameData = {
+  name: "VideoGamer Data",
+  backendLink: "https://github.com/mbisovskyi/Video-Game-Data-API",
+  frontendLink: "https://github.com/mbisovskyi/Video-Game--front",
+  image: videoGameDataImage,
+};
+
+const projectsData = [musicLibrary, youtubeClone, videoGameData];
 
 export default projectsData;
