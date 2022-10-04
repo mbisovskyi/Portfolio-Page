5acbcc0af9675cc5e458df2ec51bd93377ba1021
MBisovskyi <mbisovskyi@gmail.com>
2022-10-04 12:55:42 -0400

    Feature: installed emailjs-com and added "Send email" form to the contacts page

--- src/pages/ContactsPage/ContactsPage.js
+++ src/pages/ContactsPage/ContactsPage.js
@@ -1,7 +1,43 @@
 import "./ContactsPage.css";
+import emailjs from "emailjs-com";
 
 const ContactsPage = () => {
-  return <div>ContactsPage</div>;
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_0r69jj9",
+        "template_dm1nsio",
+        e.target,
+        "L9JXLkxo-smp7r4Jx"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
+  return (
+    <div>
+      <form onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="name" />
+        <label>Email</label>
+        <input type="email" name="email" />
+        <label>Subject</label>
+        <input type="text" name="subject" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
+      </form>
+    </div>
+  );
 };
 
 export default ContactsPage;
